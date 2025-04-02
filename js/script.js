306a45225c881b18a6339d0fbf87211a45070d3b
oninotna <antoninomusarratubbi@gmail.com>
2025-04-02 15:41:43 +0200

    convert numbers in message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 // RACCOLTA DATI
+const multipleOfThree = "Fizz";
+const multipleOfFive = "Buzz";
+const multipleOfThreeAndFive = "FizzBuzz";
 
 
 // SVOLGIMENTO
 for (i=1; i <= 100; i++) {
-    console.log(i);
-    
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(multipleOfThreeAndFive);    
+    }
+    else if (i % 3 === 0) {
+        console.log(multipleOfThree);    
+    }
+    else if (i % 5 === 0) {
+        console.log(multipleOfFive);    
+    }
+    else {
+      console.log(i);  
+    }   
 }
+
+
