2aae9a98ee7273f8e6fcac4b6fe845e498b950ef
oninotna <antoninomusarratubbi@gmail.com>
2025-04-02 16:34:10 +0200

    cconvert numbers in a message with ternary operator

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
-// RACCOLTA DATI
+// * RACCOLTA DATI
 const multipleOfThree = "Fizz";
 const multipleOfFive = "Buzz";
 const multipleOfThreeAndFive = "FizzBuzz";
 
 
-// SVOLGIMENTO
+// * SVOLGIMENTO 1
+// for (i=1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(multipleOfThreeAndFive);    
+//     }
+//     else if (i % 3 === 0) {
+//         console.log(multipleOfThree);    
+//     }
+//     else if (i % 5 === 0) {
+//         console.log(multipleOfFive);    
+//     }
+//     else {
+//       console.log(i);  
+//     }   
+// }
+
+// * SVOLGIMENTO 2
+// for (i=1; i <= 100; i++) {
+//     i % 3 === 0 && i % 5 === 0 ? console.log(multipleOfThreeAndFive)
+//     : i % 3 === 0 ? console.log(multipleOfThree)
+//     : i % 5 === 0 ? console.log(multipleOfFive) 
+//     : console.log(i);
+// }
+
+// * SVOLGIMENTO 3
 for (i=1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(multipleOfThreeAndFive);    
-    }
-    else if (i % 3 === 0) {
-        console.log(multipleOfThree);    
-    }
-    else if (i % 5 === 0) {
-        console.log(multipleOfFive);    
-    }
-    else {
-      console.log(i);  
-    }   
+    
 }
-
-
